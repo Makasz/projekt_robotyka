b9abd81d491b2f3d39010c6977f4c9b2b1a03098
Mikołaj Leśny <MakaszML@gmail.com>
2016-11-21 23:30:55 +0100

    first demo

--- renderer.js
+++ renderer.js
@@ -1,25 +1,50 @@
-window.addEventListener('DOMContentLoaded', function() {
-var canvas = document.getElementById('renderCanvas');
-var engine = new BABYLON.Engine(canvas, true);
+window.addEventListener('DOMContentLoaded', function(){
+   var canvas = document.getElementById('renderCanvas');
+   var engine = new BABYLON.Engine(canvas, true);
 
-var createScene = function() {
-  var scene = new BABYLON.Scene(engine);
-  var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
-  camera.setTarget(BABYLON.Vector3.Zero());
-  camera.attachControl(canvas, false);
-  var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-  var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-  sphere.position.y = 1;
-  var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
-return scene;
-};
-var scene = createScene();
+   var cone, sphere1, ground, cone1;
 
-engine.runRenderLoop(function() {
-    scene.render();
-});
+   var createScene = function(){
+       var scene = new BABYLON.Scene(engine);
+       var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+       camera.setTarget(BABYLON.Vector3.Zero());
+       camera.attachControl(canvas, false);
+       var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
-window.addEventListener('resize', function() {
-    engine.resize();
-});
+
+        cone = BABYLON.MeshBuilder.CreateCylinder("cone", {diameterTop: 1, tessellation: 40}, scene);
+       cone.position.y = 1;
+
+       sphere1 = BABYLON.MeshBuilder.CreateSphere("sphere1", {diameter: 1.2},scene);
+       sphere1.position.y = 2;
+       cone1 = BABYLON.MeshBuilder.CreateCylinder("cone1", {diameterTop: 1, tessellation: 40}, scene);
+       cone1.position.y = 1;
+       cone1.parent = sphere1;
+
+      sphere2 = BABYLON.MeshBuilder.CreateSphere("sphere2", {diameter: 1.2},scene);
+      sphere2.position.y = 1;
+      cone2 = BABYLON.MeshBuilder.CreateCylinder("cone2", {diameterTop: 1, tessellation: 40}, scene);
+      cone2.position.y = 1;
+      cone2.parent = sphere2;
+
+      sphere2.parent = cone1;
+
+
+        ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+       return scene;
+   }
+
+   var scene = createScene();
+   engine.runRenderLoop(function(){
+       scene.render();
+
+       sphere1.rotation.x = 0.8;
+       sphere1.rotation.y += 0.01;
+
+       sphere2.rotation.x = 0.8;
+       sphere2.rotation.y += 0.01;
+   });
+   window.addEventListener('resize', function(){
+       engine.resize();
+   });
 });
